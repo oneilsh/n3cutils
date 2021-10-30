bbca348c64e1223b72d0960c19be6be4c8da7f83
Shawn O'Neil <SHAWN.ONEIL@CUANSCHUTZ.EDU>
2021-10-30 05:05:23 +0000

    Autogenerated commit message at Oct 29, 2021 at 22:05PM

--- src/n3cutils/datasets/public.py
+++ src/n3cutils/datasets/public.py
@@ -1,5 +1,9 @@
 from transforms.api import TransformInput
 
 
-concept_set_members = TransformInput('ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6')
+def concept_set_members(): 
+    return TransformInput('ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6')
 
+def test_concept_set_members():
+    print concept_set_members()
+    return True
