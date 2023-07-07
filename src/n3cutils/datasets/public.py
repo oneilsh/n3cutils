b0f7aa058da03f0d13c8e1a6dde38ee49ae63434
Shawn O'Neil <shawn.oneil@cuanschutz.edu>
2023-07-07 19:54:51 +0000

    Autogenerated commit message at Jul 7, 2023 at 12:54PM

--- src/n3cutils/datasets/public.py
+++ src/n3cutils/datasets/public.py
@@ -1,3 +1,4 @@
+# as seen in jupyter environments, but I can't get the required package (foundry-transforms-lib-python) to install without depency errors
 # from foundry.transforms import Dataset
 
 # # testingss
